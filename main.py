ac9fd691f91090621cc6acda5ee9e2812ebfabf6
Gabriel Pereira <berkpereira@gmail.com>
2022-06-27 22:01:23 +0100

    Creates test.py, adds basic API functionality to main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_restful import Api, Resource
+
+app = Flask(__name__)
+api = Api(app)
+
+videos = {}
+
+class Video(Resource):
+	def get(self, video_id):
+		return videos[video_id]
+
+api.add_resource(Video, "/video/<int:video_id>")
+
+if __name__ == "__main__":
+	app.run(debug=True)
